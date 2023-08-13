5136f3ebdf050a7b3a5b25db9d52e420a621d604
patcharapol1234 <37959446+patcharapol1234@users.noreply.github.com>
2023-08-13 17:41:31 +0700

    Add files via upload

---
+++ rotary-axis/src/com/mycompany/rotary/axis/analyzer/RotaryAxisAnalyzer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rotary.axis.analyzer;
+
+/**
+ *
+ * @author patch
+ */
+public class RotaryAxisAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
